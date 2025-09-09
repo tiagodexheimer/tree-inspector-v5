56f7c34f1d13671f57cb65966c3c158f5cd1ecc0
Tiago Dexheimer <tiagodexheimer@gmail.com>
2025-09-09 10:38:42 -0300

    header

--- src/componets/Header.tsx
+++ src/componets/Header.tsx
@@ -4,12 +4,20 @@ import MenuIcon from '@mui/icons-material/Menu'
 
 export default function Header() {
     return (
-    <div className='flex w-full justify-between items-center bg-green-700 text-white'><Toolbar variant="dense">
-      <IconButton edge="start" color="inherit" aria-label="menu">
-        <MenuIcon />
-      </IconButton>
-        <h1>Tree Inspector V4</h1>
-      </Toolbar>
-    </div>
+        <div className='flex w-full justify-betweenitems-center bg-green-700 text-white'><Toolbar variant="dense">
+            <div>
+                <IconButton edge="start" color="inherit" aria-label="menu">
+                    <MenuIcon />
+                </IconButton>
+            </div>
+            <div>
+                <h1>Tree Inspector V4</h1>
+            </div>
+            <div>
+                Login
+            </div>
+
+        </Toolbar>
+        </div>
     );
 }
